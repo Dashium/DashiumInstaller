696265f67d0782373626c1505e946b81dcb3785b
Tai Studio <tai.studio@outlook.fr>
2021-12-21 19:06:50 +0100

    Update start.sh

--- setup/start.sh
+++ setup/start.sh
@@ -2,7 +2,7 @@ git pull
 
 if [ ! -d 'Dashium' ];
 then
-  git clone "https://github.com/Dashium/Dashium.git"
+  git clone "https://github.com/Dashium/Dashium"
   cd Dashium
   npm setup
 if [ -d 'Dashium' ];
