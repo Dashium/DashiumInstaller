a0c71a3282c3a4f2c65c28734bb08c0a800ac6cc
Tai Studio <tai.studio@outlook.fr>
2021-12-22 13:58:24 +0100

    Update install_DashiumOS.sh

--- setup/install_DashiumOS.sh
+++ setup/install_DashiumOS.sh
@@ -17,3 +17,16 @@ else
   cd DashiumOS/Dashium
   npm start
 fi
+
+cd /
+
+if [ ! -d 'DashiumOS/DashiumWebServer' ];
+then
+  cd DashiumOS
+  git clone "https://github.com/Dashium/DashiumWebServer"
+  cd DashiumWebServer
+  npm setup
+else
+  cd DashiumOS/DashiumWebServer
+  npm start
+fi
