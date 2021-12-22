5908db4fac536793fce5521be488c8d6265b72ea
Tai Studio <tai.studio@outlook.fr>
2021-12-22 16:56:44 +0100

    update

--- setup/install.sh
+++ setup/install.sh
@@ -6,6 +6,8 @@ current=$(pwd)
 
 cp `$current/setup/Dashium.service` /etc/systemd/system/Dashium.service
 
+systemctl enable /etc/systemd/system/Dashium.service
+
 mkdir DashiumOS
 cd DashiumOS
 git clone "https://github.com/Dashium/Dashium"
--- setup/installALL.sh
+++ setup/installALL.sh
@@ -1,1 +1,17 @@
+sudo apt install git
+
+sudo systemctl edit getty@tty1.service
+
+# REMOVE
+sudo apt remove cloud*
+
+sudo apt autoremove
+sudo apt update
+sudo apt upgrade
+
+# INSTALL
+
 sudo apt install openssh-server
+git clone https://github.com/Dashium/DashiumInstaller
+cd DashiumInstaller
+bash setup/install.sh
